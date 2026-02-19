126f9dfbc46d2e283ebb313a2235d6baf8cef166
AkanshuMittal <akanshumittal150@gmail.com>
2026-02-19 19:53:21 +0530

    Defining function for calculating total_count, total_sum and average

--- python-files-assignment/file_data_processing_logging.py
+++ python-files-assignment/file_data_processing_logging.py
@@ -13,3 +13,14 @@ def read_numbers_from_file(filename):
     print(f"Read {len(numbers)} numbers")
     
     return numbers 
+
+
+def statistics(numbers):
+    total_count  = len(numbers)
+    total_sum = sum(numbers)
+    average = total_sum / total_count if total_count > 0 else 0
+    
+    print("Computation completed")
+    
+    return total_count, total_sum, average
+
