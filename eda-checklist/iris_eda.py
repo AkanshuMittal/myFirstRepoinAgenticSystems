860cbb1e1da7d9d92da29abfb5f37c6daf2f40d9
AkanshuMittal <akanshumittal150@gmail.com>
2026-03-30 23:57:26 +0530

    Analyzing the distribution of one feature

--- eda-checklist/iris_eda.py
+++ eda-checklist/iris_eda.py
@@ -8,6 +8,8 @@ print(df.head())
 
 print(df.tail())
 
+print(df.info())
+
 print(df.shape)
 
 print(df.describe())
@@ -20,3 +22,10 @@ print(df.isnull().sum())
 print(df.dtypes)
 
 print(df.columns)
+
+# Observation: Analyzing the distribution of one feature
+import plotly.express as px
+
+fig = px.histogram(df, x='petal_length', title='Distribution of Petal Length')
+
+fig.show()
