03d92d29b96b95f8958e856a1f9fb54c6f63beeb
AkanshuMittal <akanshumittal150@gmail.com>
2026-03-31 00:13:00 +0530

    Analyzing relationships between variables and discover insights about species

--- eda-checklist/iris_eda.py
+++ eda-checklist/iris_eda.py
@@ -30,7 +30,19 @@ fig = px.histogram(df, x='petal_length', title='Distribution of Petal Length')
 
 fig.show()
 
-# Observation: Analyzing the distribution of one feature
+# Observation: Identifying the possible outliers in the dataset
 fig2 = px.box(df, y='petal_length', title='Box Plot for Outlier Detection')
 
 fig2.show()
+
+# Observation: Analyzing the relationship between two features
+
+fig_3 = px.scatter(df, x='petal_length', y='petal_width', color='species',
+           title='Petal Length vs Width')          
+
+fig_3.show()
+
+# Observation: Insights about different species
+
+## Different species show noticeable differences in their petal and sepal measurements, which helps in distinguishing them from each other.
+## Petal length and petal width appear to be strong features, as they clearly separate some species into distinct clusters and can be useful for classification.
